427d17448062dfb7033810fdd4aeed77a541c509
Ryosuke Hiramatsu <himaratsu@gmail.com>
2014-08-01 00:16:35 +0900

    ローカライズはじめました

---
+++ ATODE/StringConst.h
@@ -0,0 +1,21 @@
+//
+//  StringConst.h
+//  ATODE
+//
+//  Created by himara2 on 2014/07/31.
+//  Copyright (c) 2014年 himara2. All rights reserved.
+//
+
+#ifndef ATODE_StringConst_h
+#define ATODE_StringConst_h
+
+// Common
+#define PHOTO               @"PHOTO"
+#define CANCEL              @"CANCEL"
+
+// TOPViewController
+#define TAKE_PICTURE            @"TAKE_PICTURE"
+#define SELECT_LIBRARY          @"SELECT_LIBRARY"
+#define LOCATION_SETTING_NOTICE @"LOCATION_SETTING_NOTICE"
+
+#endif
